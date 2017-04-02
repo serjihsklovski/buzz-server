2df2dfdeff8efb808837d51822cfdcf37bf9ca0a
DaveNolt <davenolt.sh@gmail.com>
2017-04-02 18:27:39 +0300

    stable 02.04.2017

--- app.js
+++ app.js
@@ -1,1 +1,106 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
+var config = JSON.parse(fs.readFileSync('server_config.json'));
+var path = config['web-app-path'];
+
+app.get('/', function (req, res) {
+  var options = {
+    root: path,
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+  res.sendFile('index.html', options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent: index.html');
+    }
+  });
+});
+
+app.get('/json_links/:file', function (req, res) {
+  var options = {
+    root: path + '/app/json_links/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  var fileName = req.params.file;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.get('/generated/:file', function (req, res) {
+  var options = {
+    root: path + '/generated/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  var fileName = req.params.file;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.get('/css/:file', function (req, res) {
+  var options = {
+    root: path + '/css/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  var fileName = req.params.file;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.get('/img-refs/:file', function (req, res) {
+  var options = {
+    root: path + '/img-refs/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+  var fileName = req.params.file;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.listen(8080);
